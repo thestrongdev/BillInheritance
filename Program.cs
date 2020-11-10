806ce4826cf293e99b580ced836b78468497c92d
Candace <goodsoncj@gmail.com>
2020-11-10 12:57:23 -0500

    use other constructor

--- Program.cs
+++ Program.cs
@@ -7,18 +7,18 @@ namespace BillInheritance
         static void Main(string[] args)
         {
 
-            var myBill = new Bill();
+            var myBill = new Bill(15.80, .06);
 
-            myBill.Subtotal = 15.80;
-            myBill.TaxRate = .06;
+            //myBill.Subtotal = 15.80;
+            //myBill.TaxRate = .06;
 
             pay(myBill);
 
-            var myTippedBill = new TippableBill();
+            var myTippedBill = new TippableBill(8.5, .06, 2.0);
 
-            myTippedBill.Subtotal = 8.50;
-            myTippedBill.TaxRate = .06;
-            myTippedBill.Tip = 2.00;
+            //myTippedBill.Subtotal = 8.50;
+            //myTippedBill.TaxRate = .06;
+            //myTippedBill.Tip = 2.00;
 
             pay(myTippedBill);
             
